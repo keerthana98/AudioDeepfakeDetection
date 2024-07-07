889947c09c8c972d76f6e5a6e17899a0a83b679b
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-07-07 10:35:28 -0400

    Add LowLevelFeatureExtractor class for extracting detailed low-level audio features

---
+++ src/features/low_level_features_extractor.py
@@ -0,0 +1,60 @@
+import librosa
+import parselmouth
+from parselmouth.praat import call
+
+import numpy as np
+from tqdm import tqdm
+
+from .spectral_features import SpectralFeatureExtractor
+from .prosodic_features import ProsodicFeatureExtractor
+from .features_list import DEFAULT_FEATURES
+
+
+class LowLevelFeatureExtractor:
+    
+    def __init__(self, target_sr=16000, include_only=None, spectral_features=None, prosodic_features=None):
+        self.target_sr = target_sr
+        self.include_spectral = True
+        self.include_prosodic = True
+        self.spectral_features = spectral_features if spectral_features is not None else DEFAULT_FEATURES['spectral']
+        self.prosodic_features = prosodic_features if prosodic_features is not None else DEFAULT_FEATURES['prosodic']
+
+        if include_only is not None:
+            self.include_spectral = 'spectral' in include_only
+            self.include_prosodic = 'prosodic' in include_only
+            
+            if not self.include_spectral:
+                self.spectral_features = []
+            
+            if not self.include_prosodic:
+                self.prosodic_features = []
+                
+
+    def resample_audio(self, audio_arr, orig_sr):
+        return librosa.resample(audio_arr, orig_sr=orig_sr, target_sr=self.target_sr)
+
+    def extract_features(self, row):
+        audio_id = row['audio_id']
+        audio_arr = row['audio_arr']
+        orig_sr = row['srate']
+        real_or_fake = row['real_or_fake']
+
+        y = self.resample_audio(audio_arr, orig_sr)
+        
+        features = {}
+        
+        if self.include_spectral:
+                spectral_extractor = SpectralFeatureExtractor(y, self.target_sr)
+                features.update(spectral_extractor.extract(self.spectral_features))
+
+        if self.include_prosodic:
+            prosodic_extractor = ProsodicFeatureExtractor(y, self.target_sr, audio_arr, orig_sr)
+            features.update(prosodic_extractor.extract(self.prosodic_features))
+
+        features = {**{'audio_id': audio_id, 'real_or_fake': real_or_fake}, **features}
+
+        return features
+
+    def low_level_feature_generator(self, df):
+        for _, row in tqdm(df.iterrows(), total=len(df), desc="Processing Audios"):
+            yield self.extract_features(row)
