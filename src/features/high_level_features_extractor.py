cf042aec4ee8e405dfe6c689f77cb25bb97dbb6b
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-07-07 10:35:56 -0400

    Add HighLevelFeatureExtractor class for computing high-level audio features

---
+++ src/features/high_level_features_extractor.py
@@ -0,0 +1,95 @@
+import numpy as np
+from .stat_measures import StatisticalMeasures
+from .features_list import ALL_FEATURES
+
+
+class HighLevelFeatureExtractor:
+    def __init__(self, measures=None):
+        self.measures = measures
+
+    def compute_high_level_features(self, feature_dict):
+        features = {
+            'audio_id': feature_dict['audio_id'],
+            'real_or_fake': feature_dict['real_or_fake']
+        }
+
+        # Compute Spectral Features
+        features.update(self._compute_spectral_features(feature_dict, ALL_FEATURES['spectral']))
+            
+        # Compute Prosodic Features
+        features.update(self._compute_prosodic_features(feature_dict, ALL_FEATURES['prosodic']))
+
+        return features
+    
+    # Compute Spectral Features
+    def _compute_spectral_features(self, feature_dict, spectral_features):
+        spectral_features_dict = {}
+        for feature_name in spectral_features:
+            if feature_name in ['mfccs', 'chroma_stft']:
+                continue
+            feature_array = feature_dict.get(feature_name)
+            if feature_array is not None:
+                stats = StatisticalMeasures.compute_statistical_measures(feature_array, self.measures)
+                spectral_features_dict.update({f"{feature_name}_{key}": value for key, value in stats.items()})
+                
+        # Compute MFCC Features
+        if 'mfccs' in ALL_FEATURES['spectral'] and 'mfccs' in feature_dict:
+            spectral_features_dict.update(self._compute_mfcc_features(feature_dict['mfccs']))
+            
+        # Compute Chroma Features
+        if 'chroma_stft' in ALL_FEATURES['spectral'] and 'chroma_stft' in feature_dict:
+            spectral_features_dict.update(self._compute_chroma_features(feature_dict['chroma_stft']))
+            
+        return spectral_features_dict
+    
+    # Compute MFCC Features
+    def _compute_mfcc_features(self, mfccs_flat):
+        mfccs = mfccs_flat.reshape(-1, 13)  # Reshape the flattened array to its original form
+        mfcc_features_dict = {}
+        for i in range(mfccs.shape[1]):
+            feature_array = mfccs[:, i]
+            stats = StatisticalMeasures.compute_statistical_measures(feature_array, self.measures)
+            mfcc_features_dict.update({f"mfcc_{i+1}_{key}": value for key, value in stats.items()})
+        return mfcc_features_dict
+    
+    # Compute Chroma Features
+    def _compute_chroma_features(self, chroma_flat):
+        chroma = chroma_flat.reshape(-1, 12)  # Reshape the flattened array to its original form
+        chroma_features_dict = {}
+        for i in range(chroma.shape[1]):
+            feature_array = chroma[:, i]
+            stats = StatisticalMeasures.compute_statistical_measures(feature_array, self.measures)
+            chroma_features_dict.update({f"chroma_{i+1}_{key}": value for key, value in stats.items()})
+        return chroma_features_dict
+    
+    # Compute Prosodic Features
+    def _compute_prosodic_features(self, feature_dict, prosodic_features):
+        prosodic_features_dict = {}
+        
+        for feature_name in prosodic_features:
+            if feature_name in ['speaking_rate', 'pauses']:
+                continue
+            feature_array = feature_dict.get(feature_name)
+            if feature_array is not None:
+                stats = StatisticalMeasures.compute_statistical_measures(feature_array, self.measures)
+                prosodic_features_dict.update({f"{feature_name}_{key}": value for key, value in stats.items()})
+
+        if 'speaking_rate' in prosodic_features and 'speaking_rate' in feature_dict:
+            prosodic_features_dict['speaking_rate'] = feature_dict['speaking_rate']
+
+        if 'pauses' in prosodic_features and 'pauses' in feature_dict:
+            pauses = feature_dict['pauses']
+            if pauses:
+                pause_durations = np.array([end - start for start, end in pauses])
+                pause_stats = StatisticalMeasures.compute_statistical_measures(pause_durations, self.measures)
+                prosodic_features_dict.update({f"pause_{key}": value for key, value in pause_stats.items()})
+            else:
+                for measure in self.measures:
+                    prosodic_features_dict[f'pause_{measure}'] = np.nan
+
+        return prosodic_features_dict
+
+
+    def high_level_feature_generator(self, low_level_gen):
+        for low_level_features in low_level_gen:
+            yield self.compute_high_level_features(feature_dict=low_level_features)
