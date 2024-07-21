a601fc3988c48602f924d5ea5a4a8bb8299138fe
Ajay Karthick Senthil Kumar <24907339+iajaykarthick@users.noreply.github.com>
2024-07-21 13:49:12 -0400

    Update data loader to support wavefake dataset and improve code organization

--- src/data/load/data_loader.py
+++ src/data/load/data_loader.py
@@ -1,23 +1,34 @@
-from .codecfake import fetch_audio_id_list, get_audio_dataset, get_dataset_from_single_parquet
+from . import codecfake, wavefake
+# fetch_audio_id_list, get_audio_dataset, get_dataset_from_single_parquet
 
+
+def get_wavefake_audio_id_list():
+    """
+    Fetch the list of audio IDs in the wavefake dataset.
+    """
+    return wavefake.fetch_audio_id_list()
 
 def get_codecfake_audio_id_list():
     """
     Fetch the list of audio IDs in the codecfake dataset.
     """
-    return fetch_audio_id_list()
+    return codecfake.fetch_audio_id_list()
 
 def load_audio_data(audio_ids, dataset='codecfake', cache_dir=None):
     """
     Load audio data for the given audio IDs.
     """
     if dataset == 'codecfake':
-        return get_audio_dataset(audio_ids, cache_dir=cache_dir)
+        return codecfake.get_audio_dataset(audio_ids, cache_dir=cache_dir)
     else:
         raise ValueError(f"Invalid dataset: {dataset}") 
     
-def load_parquet_data(partition_id, cache_dir=None):
+def load_parquet_data(partition_id, dataset='codecfake', cache_dir=None):
     """
     Load audio data from a single parquet file.
     """
-    return get_dataset_from_single_parquet(partition_id, cache_dir=cache_dir)
+    if dataset == 'codecfake':
+        return codecfake.get_dataset_from_single_parquet(partition_id, cache_dir=cache_dir)
+    if dataset == 'wavefake':
+        return wavefake.get_dataset_from_single_parquet(partition_id, cache_dir=cache_dir)
+    # return get_dataset_from_single_parquet(partition_id, cache_dir=cache_dir)
